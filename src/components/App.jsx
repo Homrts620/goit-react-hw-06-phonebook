2c84a2e55c1f00d502c9219bae401f65f1be48ec
Homrts620 <hom620@gmail.com>
2024-06-01 09:48:48 +0200

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 import css from './App.module.css';
-import ContactAdd from './ContactAdd/ContactAdd';
-import ContactList from './ContactList/ContactList';
+import { ContactAdd } from './ContactAdd/ContactAdd';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { getContacts } from './redux/state';
 
 
-const App = () => {
-  const [contacts, setContacts] = useState([
-  ]);
+const key = 'Contacts';
 
+export const App = () => {
+  const contacts = useSelector(getContacts);
   const isMounted = useRef(false);
-
-  const Key = 'Contacts';
-
-  useEffect(() => {
-    const savedContacts = JSON.parse(localStorage.getItem(Key));
-    savedContacts && setContacts([...savedContacts]);
-  }, []);
 
   useEffect(() => {
     if (isMounted.current) {
@@ -25,34 +21,6 @@ const App = () => {
     }
   }, [contacts]);
 
-  const checkContact = newContact => {
-    const isInBase = contacts.some(
-      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-    );
-    return isInBase;
-  };
-
-  const addNewContact = newContact => {
-    const check = checkContact(newContact);
-    if (!check) {
-      let actualContacts = contacts;
-      actualContacts.push(newContact);
-      setContacts([...actualContacts]);
-    } else {
-      alert(`${newContact.name} is already in contacts`);
-    }
-  };
-
-  const deleteUser = e => {
-    const index = contacts.findIndex(contact => contact.id === e.target.id);
-    if (index !== -1) {
-      const updatedContacts = [...contacts];
-      updatedContacts.splice(index, 1);
-      setContacts(updatedContacts);
-    } else {
-      alert(`Contact ${e.target.id} not found.`);
-    }
-  };
   return (
     <div className={css.primary}>
       <h1 className={css.header}> Phonebook</h1>
@@ -64,5 +32,3 @@ const App = () => {
     </div>
   );
 };
-
-export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,33 +1,37 @@
-import PropTypes from 'prop-types';
-import Contact from './Contact/Contact';
+import { deleteContact } from 'components/redux/contactsSlice';
+import { getContacts, getFilter } from 'components/redux/state';
+import { useDispatch, useSelector } from 'react-redux';
 import css from './ContactList.module.css';
 
 
-const ContactList =   ({ contacts, deleteFunction }) => {
-    return(
-<div className={css.primary}>
+export const ContactList = () => {
+    const contacts = useSelector(getContacts);
+    const filter = useSelector(getFilter);
+    const dispatch = useDispatch();
+
+    const handleDelete = e => {
+        dispatch(deleteContact(e.target.id));
+};
+    
+return(
     <ul className={css.list}>
-        {contacts.map(contact => (
-            <Contact key = {contact.id}>
-                {contact.name} : {contact.number} {''}
-                <button 
-                id = {contact.id} 
-                onClick={deleteFunction} 
+    {contacts
+        .filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map(contact => {
+        return (
+            <li key={contact.id}>
+            {contact.name} : {contact.number}{' '}
+            <button
+                id={contact.id}
+                onClick={handleDelete}
                 className={css.button}>
                 Delete
-                </button>
-            </Contact>
-        ))
-        }
+            </button>
+            </li>
+        );
+        })}
     </ul>
-</div>
 );
 };
-
-
-ContactList.propTypes = {
-    contact: PropTypes.array.isRequired,
-    deleteFunction: PropTypes.func.isRequired,
-};
-
-export default ContactList;
